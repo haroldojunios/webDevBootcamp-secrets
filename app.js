03be7fcedaa6e2a7edb00e92453bd7f7e3c82853
Haroldo Júnio <haroldojunios@gmail.com>
2021-12-10 09:07:32 -0300

    Autenticação de nível 3
    Hashing usando md5

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -20,8 +20,6 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
-
 const User = mongoose.model("User", userSchema);
 
 app.get("/", function (req, res) {
@@ -35,7 +33,7 @@ app.route("/login")
 
   .post(function (req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({ email: username }, function (err, user) {
       if (err) {
@@ -56,9 +54,12 @@ app.route("/register")
   })
 
   .post(function (req, res) {
+    const username = req.body.username;
+    const password = md5(req.body.password);
+
     const newUser = new User({
-      email: req.body.username,
-      password: req.body.password
+      email: username,
+      password: password
     });
 
     newUser.save(function (err) {
