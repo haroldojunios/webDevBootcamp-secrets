236d0204f30cbd7feafb3c6f22b3e4e901aabc12
Haroldo Júnio <haroldojunios@gmail.com>
2021-12-09 22:02:57 -0300

    Criação de rotas básicas de get (home, login, register)

--- app.js
+++ app.js
@@ -10,6 +10,18 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
