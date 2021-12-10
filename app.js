44e107a6cafb123bbd520328e76ec2beaa6ca0af
Haroldo Júnio <haroldojunios@gmail.com>
2021-12-09 22:21:07 -0300

    Autenticação de nível 1
    Registro de usuário e senha em plain text

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -10,17 +11,56 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+
+const userSchema = { email: String, password: String };
+const User = mongoose.model("User", userSchema);
+
 app.get("/", function (req, res) {
   res.render("home");
 });
 
-app.get("/login", function (req, res) {
-  res.render("login");
-});
+app.route("/login")
+  .get(function (req, res) {
+    res.render("login");
+  })
 
-app.get("/register", function (req, res) {
-  res.render("register");
-});
+  .post(function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({ email: username }, function (err, user) {
+      if (err) {
+        console.log(err);
+      } else {
+        if (user) {
+          if (user.password === password) {
+            res.render("secrets");
+          }
+        }
+      }
+    });
+  });
+
+app.route("/register")
+  .get(function (req, res) {
+    res.render("register");
+  })
+
+  .post(function (req, res) {
+    const newUser = new User({
+      email: req.body.username,
+      password: req.body.password
+    });
+
+    newUser.save(function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
+  });
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
